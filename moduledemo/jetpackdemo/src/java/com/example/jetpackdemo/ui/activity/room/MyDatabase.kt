e3c18169526029de342f63d41aee6ef17833145b
yangyang <yangyang@yax.xyz>
2023-04-24 20:48:12 +0800

    MOD:使用Migration升级数据库

--- moduledemo/jetpackdemo/src/java/com/example/jetpackdemo/ui/activity/room/MyDatabase.kt
+++ moduledemo/jetpackdemo/src/java/com/example/jetpackdemo/ui/activity/room/MyDatabase.kt
@@ -18,7 +18,7 @@ import java.com.example.jetpackdemo.interfaces.StudentDao
  * Create by Yang Yang on 2023/4/23
  */
 
-@Database(entities = [Student::class], version = 3, exportSchema = false)
+@Database(entities = [Student::class], version = 4, exportSchema = false)
 abstract class MyDatabase : RoomDatabase() {
 
     companion object {
@@ -26,9 +26,12 @@ abstract class MyDatabase : RoomDatabase() {
         var instance: MyDatabase? = null
 
         /**
-         * allowMainThreadQueries() ：是否允许在主线程中查询
+         * 1.allowMainThreadQueries() ：是否允许在主线程中查询
          * TODO Room不允许在主线程中执行操作
          * 如果非要在主线程中执行使用allowMainThreadQueries()
+         *
+         * 2.fallbackToDestructiveMigration()
+         * 在出现升级异常时,重建数据表,同时数据也会丢失。
          */
         @Synchronized
         fun getInstance(context: Context): MyDatabase {
@@ -38,19 +41,20 @@ abstract class MyDatabase : RoomDatabase() {
                     MyDatabase::class.java,
                     "my_database.db")
                     //.allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
                     .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
                     .build()
             }
             return instance!!
         }
 
-        val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+        private val MIGRATION_1_2: Migration = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 database.execSQL("ALTER TABLE student ADD COLUMN sex INTEGER NOT NULL DEFAULT 1")
             }
         }
 
-        val MIGRATION_2_3: Migration = object : Migration(2, 3) {
+        private val MIGRATION_2_3: Migration = object : Migration(2, 3) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 database.execSQL("ALTER TABLE student ADD COLUMN grade INTEGER NOT NULL DEFAULT 1")
             }
