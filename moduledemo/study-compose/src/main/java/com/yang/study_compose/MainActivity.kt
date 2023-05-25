cec5c34ab08bba36157f7e1f3e08b830b4922500
yangyang <yangyang@yax.xyz>
2023-05-25 20:33:08 +0800

    MOD:ModifierBackground

--- moduledemo/study-compose/src/main/java/com/yang/study_compose/MainActivity.kt
+++ moduledemo/study-compose/src/main/java/com/yang/study_compose/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,8 +17,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,7 +40,10 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Greeting("Android & YANGYANG")
 
-                    ModifierSize()
+                    Column {
+                        ModifierSize()
+                        ModifierBackground()
+                    }
                 }
             }
         }
@@ -46,7 +55,7 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 fun ModifierSize() {
     JetPackTheme {
@@ -76,6 +85,37 @@ fun ModifierSize() {
                     .size(100.dp, 300.dp)
                     .clip(CircleShape)
             )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ModifierBackground() {
+
+    val verticalGradientBrush = Brush.verticalGradient(
+        colors = listOf(
+            Color.Blue,
+            Color.Cyan,
+            Color.Yellow
+        )
+    )
+    JetPackTheme {
+        Row {
+            Box(modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Blue)
+            ) {
+                Text(text = "纯色", Modifier.align(Alignment.Center))
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Box(modifier = Modifier
+                .size(100.dp)
+                .background(brush = verticalGradientBrush)
+            ) {
+                Text(text = "渐变色", Modifier.align(Alignment.Center))
+            }
         }
     }
 }
