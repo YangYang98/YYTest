de1dadc7f9712af5d8c8bb6f3f75f25b56a03e12
yangyang <yangyang@yax.xyz>
2023-06-09 11:55:59 +0800

    MOD:Jetpack Compose --> 状态的持久化与恢复

---
+++ moduledemo/study-compose/src/main/java/com/yang/study_compose/data/City.kt
@@ -0,0 +1,7 @@
+package com.yang.study_compose.data
+
+
+/**
+ * Create by Yang Yang on 2023/6/9
+ */
+data class City(val name: String, val country: String)
--- moduledemo/study-compose/src/main/java/com/yang/study_compose/ui/activity/StateManagerActivity.kt
+++ moduledemo/study-compose/src/main/java/com/yang/study_compose/ui/activity/StateManagerActivity.kt
@@ -16,10 +16,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.SaverScope
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.mapSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.yang.study_compose.data.City
 
 
 /**
@@ -31,7 +37,12 @@ class StateManagerActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            CounterScreen()
+
+            Column {
+                CounterScreen()
+
+                CounterScreen2()
+            }
         }
     }
 }
@@ -57,6 +68,32 @@ fun CounterScreen() {
             counter --
         }
     }
+}
+
+@Composable
+fun CounterScreen2() {
+
+    /**
+     * TODO rememberSavable
+     *
+     * rememberSavable，它可以像Activity的onSaveInstanceState那样在进程被杀死时自动保存状态，
+     * 同时像onRestoreInstanceState一样随进程重建而自动恢复。
+     * 能跨越Activity或者跨越进程存在。比如当横竖屏等ConfigurationChanged事件发生时，状态会发生丢失。
+     */
+
+    var counter by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    CounterComponent(
+        counter = counter, { counter++ }
+    ) {
+        if (counter > 0) {
+            counter --
+        }
+    }
+
+
 }
 
 
@@ -98,4 +135,71 @@ fun CounterComponent(
             }
         }
     }
+}
+
+
+@Composable
+fun TestSaver() {
+
+    val selectedCity = rememberSaveable(stateSaver = CitySaver) {
+        mutableStateOf(City("Barcelona", "Spain"))
+    }
+
+
+    /**
+     * TODO MapSaver
+     *
+     * MapSaver将对象转换为Map<String, Any>的结构进行保存，注意value是可保存到Bundle的类型(序列化)
+     */
+    val CitySaver2 = run {
+        val nameKey = "Name"
+        val countryKey = "Country"
+
+        mapSaver(
+            save = { mapOf(nameKey to it.name, countryKey to it.country) },
+            restore = { City(it[nameKey] as String, it[countryKey] as String) }
+        )
+    }
+
+    val selectedCity2 = rememberSaveable(stateSaver = CitySaver2) {
+        mutableStateOf(City("Barcelona", "Spain"))
+    }
+
+    /**
+     * TODO ListSaver
+     *
+     * ListSaver则是将对象转换为List<Any>的数据结构进行保存。
+     */
+    val CitySaver3 = listSaver<City, Any>(
+        save = { listOf(it.name, it.country) },
+        restore = { City(it[0] as String, it[1] as String) }
+    )
+
+    val selectedCity3 = rememberSaveable(stateSaver = CitySaver3) {
+        mutableStateOf(City("Barcelona", "Spain"))
+    }
+}
+
+/**
+ * TODO Saver
+ *
+ * 有的数据结构可能无法添加Parcelable接口，比如定义在三方库的类等，
+ * 此时可以通过自定义Saver为其实现保存和恢复的逻辑。只需要在调用rememberSavable时传入此Saver即可
+ */
+object CitySaver : Saver<City, Bundle> {
+    override fun restore(value: Bundle): City? {
+        return value.getString("name")?.let { name ->
+            value.getString("country")?.let { country ->
+                City(name, country)
+            }
+        }
+    }
+
+    override fun SaverScope.save(value: City): Bundle? {
+        return Bundle().apply {
+            putString("name", value.name)
+            putString("country", value.country)
+        }
+    }
+
 }
